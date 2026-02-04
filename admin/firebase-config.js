c74457a60fefab2a65fb3129fdf2d832e6c4f757
emaildolixodanet-cloud <emaildolixodanet@gmail.com>
2026-02-04 21:35:04 +0000

    Add Firebase configuration file

---
+++ admin/firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyC2l...",
+  authDomain: "dripdropshop.firebaseapp.com",
+  projectId: "dripdropshop",
+  storageBucket: "dripdropshop.appspot.com",
+  messagingSenderId: "318340828156",
+  appId: "1:318340828156:web:2eaf777660edbea008079"
+};
