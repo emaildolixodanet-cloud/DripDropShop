6acbd34ddff43ad7b996d8cd36f73f461524f24f
emaildolixodanet-cloud <emaildolixodanet@gmail.com>
2026-02-04 22:00:45 +0000

    Update product details for hoodies

--- products.js
+++ products.js
@@ -1,42 +1,44 @@
-// products.js
+// products.js - Catálogo de Produtos
 const products = [
+  {
+    id: "ralph-lauren-hoodie",
+    type: "Hoodie",
+    name: "Hoodie Ralph Lauren",
+    price: "35,00 €",
+    description: "Artigo novo com etiqueta.",
+    available: true,
+    whatsappText: "Olá, estou interessado no Hoodie Ralph Lauren.",
+    colors: [
+      {
+            "name": "azul",
+            "images": [
+                  "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-azul/ralph-lauren-hoodie-azul-01.jpg",
+                  "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-azul/ralph-lauren-hoodie-azul-02.jpg",
+                  "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-azul/ralph-lauren-hoodie-azul-03.jpg"
+            ]
+      },
+      {
+            "name": "branca",
+            "images": [
+                  "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-branca/ralph-lauren-hoodie-branca-01.jpg",
+                  "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-branca/ralph-lauren-hoodie-branca-02.jpg",
+                  "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-branca/ralph-lauren-hoodie-branca-03.jpg",
+                  "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-branca/ralph-lauren-hoodie-branca-04.jpg"
+            ]
+      }
+]
+  },
+
   {
     id: "hoodie-carhartt",
     type: "Hoodie",
     name: "Hoodie Carhartt",
     price: "35,00 €",
     description: "Artigo novo com etiqueta.",
-    image: [
-      "img/produtos/hoodie-carhartt/hoodie-carhartt-01.jpg",
-      "img/produtos/hoodie-carhartt/hoodie-carhartt-02.jpg",
-      "img/produtos/hoodie-carhartt/hoodie-carhartt-03.jpg",
-      "img/produtos/hoodie-carhartt/hoodie-carhartt-04.jpg",
-      "img/produtos/hoodie-carhartt/hoodie-carhartt-05.jpg"
-    ],
     available: true,
-    whatsappText: "Olá, estou interessado no Hoodie Carhartt. Ainda está disponível?"
+    whatsappText: "Olá, estou interessado no Hoodie Ralph Lauren.",
+    image: ["img/produtos/hoodie-carhartt/hoodie-carhartt-castanha/hoodie-carhartt-castanha-01.jpg","img/produtos/hoodie-carhartt/hoodie-carhartt-castanha/hoodie-carhartt-castanha-02.jpg","img/produtos/hoodie-carhartt/hoodie-carhartt-castanha/hoodie-carhartt-castanha-03.jpg","img/produtos/hoodie-carhartt/hoodie-carhartt-castanha/hoodie-carhartt-castanha-04.jpg","img/produtos/hoodie-carhartt/hoodie-carhartt-castanha/hoodie-carhartt-castanha-05.jpg"]
   },
-
-
-  {
-  id: "ralph-lauren-hoodie",
-  type: "Hoodie",
-  name: "Hoodie Ralph Lauren",
-  price: "50,00 €",
-  description: "Artigo novo com etiqueta.",
-  image: [
-    "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-01.jpg",
-    "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-02.jpg",
-    "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-03.jpg",
-    "img/produtos/ralph-lauren-hoodie/ralph-lauren-hoodie-04.jpg"
-  ],
-  available: true, // Muda para false para aparecer "Sem stock"
-  whatsappText: "Olá, estou interessado no Hoodie Ralph Lauren. Ainda está disponível?"
-},
-
-
-
-
-
 ];
 
+window.products = products;
