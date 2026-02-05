74b5338df2ee90f92b0901f12acab3afb02babfd
emaildolixodanet-cloud <emaildolixodanet@gmail.com>
2026-02-05 02:09:43 +0000

    Delete admin/firebase-config.js

--- admin/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyC2luI4ptngogaIuK3qQ3wy5rgzYep2a60",
-  authDomain: "dripdropshop.firebaseapp.com",
-  projectId: "dripdropshop",
-  storageBucket: "dripdropshop.appspot.com",
-  messagingSenderId: "318340828156",
-  appId: "1:318340828156:web:2eaf777660edbea008079"
-};
